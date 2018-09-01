3c8f4e32756a707987a15dd84e7ab902233d3d9a
shivender yadav <shivendra.yadav36@gmail.com>
2018-09-01 21:15:18 +0530

    Function Overloading

---
+++ funover.java
@@ -0,0 +1,28 @@
+class funover{
+int empid;
+int sal;
+void show()
+{
+System.out.println("without arguement");
+}
+void show(int a)
+{
+	empid=a;
+	System.out.println("empid="+empid);
+}
+void show(int a,int b)
+{
+	empid=a;
+	sal=b;
+	System.out.println("empid="+empid);
+	System.out.println("sal="+sal);
+}
+
+public static void main(String[] a)
+{
+funover e1= new funover();
+e1.show();	
+e1.show(10);
+e1.show(20,30);
+}
+}
