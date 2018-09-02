e2eae47d87be8e7b1c0f9e8a72fd51e573cd59c3
shivender yadav <shivendra.yadav36@gmail.com>
2018-09-02 17:41:02 +0530

    https://github.com/shivendra036/java-programs

---
+++ const6.java
@@ -0,0 +1,20 @@
+//calling method in class level
+class const6
+{		//here x assign value of function getx() return
+	int x=getx();
+	int getx()
+	{
+		//x default value get printed that is 0
+		System.out.println(x);
+		return 20;
+	}
+	const6()
+	{
+		System.out.println(x);
+	}		
+	public static void main(String ... a)
+	{
+		//at first new will get to const6() class
+	new const6();			
+	}		
+}
