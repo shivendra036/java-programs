44b8f02139aaeeb69980bcba50d3bbb3efc6441e
shivender yadav <shivendra.yadav36@gmail.com>
2018-09-02 12:35:08 +0530

    program showing this is hidden

--- this2.java
+++ this2.java
@@ -3,7 +3,8 @@
 class this2
 {
 	int x=10;
-	/*here Temp this is hidden which will catch the Rid given by t*/
+	/*here Temp this is hidden which will catch the Rid given by t
+	  but this should be used first in function*/
 	void show(int x /*,Temp this*/)
 	{
 		//calling local variable but due to shadowing o/p x will be of main x
