1cf55d4894a811edb42fd9f77dff87a48db96597
shivender yadav <shivendra.yadav36@gmail.com>
2018-09-02 14:48:32 +0530

    object initialisation

---
+++ const1.java
@@ -0,0 +1,16 @@
+/* object initialisation */
+class const1
+{
+	int sal;
+	const1()
+	{
+		this.sal=15000;
+		//constructor can return blank value
+		System.out.println("sal= "+sal);
+	}
+	public static void main(String ... a)
+	{
+		const1 e1= new const1();			
+        
+	}	
+}
